d6a4ae48e78611964908b76845a7fa1855eeade1
Matvey <zxdfcvas4554@mail.ru>
2023-08-14 13:38:11 +0300

    Add project files.

---
+++ Task59_Backtracking_Medium_Permutations/Task59_Backtracking_Medium_Permutations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> result = {};
+
+    std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+        permutation(nums, nums.size());
+        return result;
+    }
+
+    void permutation(std::vector<int> arr, int n) {
+        if (n < 2) {
+            result.push_back(arr);
+        }
+        else {
+            for (int i = n - 1;i >= 0;i--) {
+                std::swap(arr[i], arr[n-1]);
+                permutation(arr, n - 1);
+                std::swap(arr[i], arr[n-1]);
+            }
+        }
+    }
+};
